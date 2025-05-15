97c54d22f6841a0081d227b2eac7a1486476b8ea
QRBase scan <contact@qrbase.xyz>
2025-05-15 09:48:12 +0000

    create get images qr api

---
+++ pages/api/proxyFullImages.ts
@@ -0,0 +1,75 @@
+export const runtime = "edge";
+
+import { encryptObject } from "@/src/app/utils/encrypt_decrypt"
+
+export default async function handler(req: Request){
+  try {
+    const url = new URL(req.url);
+    const apiKey = req.headers.get('api-key');
+    const apiKeyCloud = process.env.NEXT_PUBLIC_API_KEY as string;
+    const apiKeyWorker = process.env.NEXT_PUBLIC_API_KEY_WORKER as string;
+    const apiCoinSecret =  process.env.NEXT_PUBLIC_API_COIN_SECRET as string;
+    const apiFullImages =  process.env.NEXT_PUBLIC_API_FULL_IMAGES as string;
+    const body = await req.text();  // Read the stream
+  const { pool } = JSON.parse(body);  // Parse the body as JSON and extract 'pool'
+
+    if (!apiKey || apiKey !== apiKeyCloud) {
+      return new Response(JSON.stringify({ message: 'Unauthorized' }), {
+        status: 401,
+        headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'GET, POST',
+          'Access-Control-Allow-Headers': 'Content-Type, api-key',
+        },
+      });
+    }
+
+    if (!pool) {
+      return new Response(
+        JSON.stringify({ message: "Pool parameter is required" }),
+        { status: 400 }
+      );
+    }
+
+      // Handle preflight CORS
+  if (req.method === 'OPTIONS') {
+    return new Response(null, {
+      status: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST',
+        'Access-Control-Allow-Headers': 'Content-Type, api-key',
+      },
+    });
+  }
+  if (req.method === 'POST') {
+    const requestBody = { pool };
+ const workerResponse = await fetch(apiFullImages, {
+  method: "POST",
+  headers: { "Content-Type": "application/json" ,"Cache-Control": "no-cache", "api-key":  apiKeyWorker },
+  body: JSON.stringify(requestBody),
+
+});
+ const workerData = await workerResponse.json();
+
+ return new Response(JSON.stringify(encryptObject(workerData , apiCoinSecret)), {
+   status: 200,
+   headers: {
+     "Content-Type": "application/json",
+     "Cache-Control": "no-cache",
+     "Access-Control-Allow-Origin": "*",
+   },
+ });
+  }
+   
+  } catch (error) {
+    console.error("Error in proxyFullImages API:", error);
+    return new Response(
+      JSON.stringify({ error: "Internal Server Error" }),
+      { status: 500, headers: { "Content-Type": "application/json" } }
+    );
+  }
+}
+
+
