bdea4fc76ab7263bd6e92c207a836873ee5201e0
QRBase scan <contact@qrbase.xyz>
2025-05-16 00:22:47 +0000

    create the banner bar in the top

---
+++ src/app/components/sections/QrBaseBanner.tsx
@@ -0,0 +1,17 @@
+import { FaBullhorn } from 'react-icons/fa';
+
+
+export function QrBaseBanner() {
+  return (
+<div className="z-50 fixed top-0 left-0 flex h-10 xs:h-11 w-full flex-wrap items-center justify-center sm:h-13
+  bg-gradient-to-r from-[#50DEF5] via-[#0052FF] to-[#AE80FF] text-white">
+  <div className="flex h-full flex-wrap items-center justify-center text-center text-xs sm:text-sm">
+    <FaBullhorn className="text-white w-4 h-4" />
+    <span className="ml-2 text-white">
+      Round 1 is LIVE!
+    </span>
+  </div>
+</div>
+
+  );
+}
