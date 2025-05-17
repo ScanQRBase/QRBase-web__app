6415d9aaab881928f665c4993def5534fc00cdb5
Ahmed Abdelkafi <ahmed.abdelkafi@bitgrass.com>
2025-05-17 01:02:16 +0000

    read image seo

--- src/app/components/sections/modals/ShareModal.tsx
+++ src/app/components/sections/modals/ShareModal.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { X } from "lucide-react";
 import { motion } from "framer-motion";
 import XIcon from "@/src/app/images/svg/socialMedia/XIcon";
@@ -15,14 +15,39 @@ export default function ShareModal({ partnerData, onClose, isOpen }: ShareModalP
   if (!isOpen) return null;
 
   const [copied, setCopied] = useState(false);
+  const [isLoading, setIsLoading] = useState(false)
+  const [preview, setPreview] = useState('');
   const currentUrl = typeof window !== "undefined" ? window.location.href : "";
-  const shareText = `Checkout $${partnerData?.title} on @qrbase`;
+  const shareTextTwitter = `Checkout $${partnerData?.title} on @ScanQRBase`;
+  const shareTextWarpcast = `Checkout $${partnerData?.title} on @scanqrbase.eth`;
 
-  const encodedText = encodeURIComponent(shareText);
+
+  const encodedTextTwitter = encodeURIComponent(shareTextTwitter);
+  const encodedTextWarpcast = encodeURIComponent(shareTextWarpcast);
+
   const encodedLink = encodeURIComponent(currentUrl);
 
-  const twitterUrl = `https://x.com/intent/post?text=${encodedText}%0A%0A${encodedLink}`;
-  const warpcastUrl = `https://warpcast.com/~/compose?text=${encodedText}&embeds[]=${encodedLink}`;
+  const twitterUrl = `https://x.com/intent/post?text=${encodedTextTwitter}%0A%0A${encodedLink}`;
+  const warpcastUrl = `https://warpcast.com/~/compose?text=${encodedTextWarpcast}&embeds[]=${encodedLink}`;
+
+
+  useEffect(() => {
+    if (!isOpen) return;
+    setIsLoading(true)
+    fetch(`https://api.microlink.io/?url=${encodeURIComponent(currentUrl)}`)
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.status === "success") {
+          const image = data.data.image?.url as string
+          setPreview(
+            data.data.image?.url,
+          );
+        }
+        setIsLoading(false)
+      })
+      .catch(() => setPreview(''));
+  }, [isOpen, currentUrl]);
+  
 
   const handleCopy = () => {
     navigator.clipboard.writeText(currentUrl);
@@ -31,7 +56,7 @@ export default function ShareModal({ partnerData, onClose, isOpen }: ShareModalP
   };
 
   return (
-    <div className="fixed inset-0 bg-black/30 z-50 flex items-center justify-center" style={{marginTop:0}}>
+    <div className="fixed inset-0 bg-black/30 z-50 flex items-center justify-center" style={{ marginTop: 0 }}>
       <div className="bg-white rounded-2xl shadow-lg p-6 w-full max-w-md relative border-4 md:max-w-lg lg:max-w-xl">
         {/* Header */}
         <div className="flex justify-between items-center mb-4">
@@ -42,13 +67,26 @@ export default function ShareModal({ partnerData, onClose, isOpen }: ShareModalP
         </div>
 
         {/* SEO Card */}
-        <div className="rounded-lg border-4 mb-6" style={{ borderColor: partnerData.PRIMARY_COLOR, borderRadius: "15px" }}>
-          <img
-            src={partnerData?.seoCard}
-            alt="SEO Card"
-            className="w-full h-48 object-cover rounded-md"
-            style={{ borderRadius: "10px" }}
-          />
+        <div className="rounded-lg mb-6" style={{ width: 'max-content', placeSelf: 'center' }}>
+          {isLoading ? (
+            <div className="flex justify-center items-center h-48 w-full">
+              {/* Simple CSS spinner */}
+              <div className="animate-spin rounded-full h-12 w-12 border-t-4 border-b-4 border-gray-900"></div>
+            </div>
+          ) : (
+            preview ? (
+              <img
+                src={preview}
+                alt="SEO Card"
+                className="w-full h-48 object-contain rounded-md"
+                style={{ borderRadius: "10px" }}
+              />
+            ) : (
+              <div className="w-full h-48 flex justify-center items-center bg-gray-100 rounded-md text-gray-400">
+                No preview available
+              </div>
+            )
+          )}
         </div>
 
         {/* Buttons */}
