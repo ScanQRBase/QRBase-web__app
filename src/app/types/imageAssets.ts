b432131528ad255f1988c30aeb385f1a11eaf2df
Ahmed Abdelkafi <ahmed.abdelkafi@bitgrass.com>
2025-05-17 01:04:01 +0000

    integrate new image

--- src/app/types/imageAssets.ts
+++ src/app/types/imageAssets.ts
@@ -40,15 +40,15 @@ import unlockedImg4_7 from '@/src/app/images/unlockedQr/0x403B1ecaC8d7AC35Dbfb8E
 import unlockedImg4_8 from '@/src/app/images/unlockedQr/0x403B1ecaC8d7AC35Dbfb8Efae59F440a6F3BEB84/Split-8.png';
 import unlockedImg4_9 from '@/src/app/images/unlockedQr/0x403B1ecaC8d7AC35Dbfb8Efae59F440a6F3BEB84/Split-9.png';
 
-import unlockedImg5_1 from '@/src/app/images/unlockedQr/0xc9034c3E7F58003E6ae0C8438e7c8f4598d5ACAA/Split-1.png';
-import unlockedImg5_2 from '@/src/app/images/unlockedQr/0xc9034c3E7F58003E6ae0C8438e7c8f4598d5ACAA/Split-2.png';
-import unlockedImg5_3 from '@/src/app/images/unlockedQr/0xc9034c3E7F58003E6ae0C8438e7c8f4598d5ACAA/Split-3.png';
-import unlockedImg5_4 from '@/src/app/images/unlockedQr/0xc9034c3E7F58003E6ae0C8438e7c8f4598d5ACAA/Split-4.png';
-import unlockedImg5_5 from '@/src/app/images/unlockedQr/0xc9034c3E7F58003E6ae0C8438e7c8f4598d5ACAA/Split-5.png';
-import unlockedImg5_6 from '@/src/app/images/unlockedQr/0xc9034c3E7F58003E6ae0C8438e7c8f4598d5ACAA/Split-6.png';
-import unlockedImg5_7 from '@/src/app/images/unlockedQr/0xc9034c3E7F58003E6ae0C8438e7c8f4598d5ACAA/Split-7.png';
-import unlockedImg5_8 from '@/src/app/images/unlockedQr/0xc9034c3E7F58003E6ae0C8438e7c8f4598d5ACAA/Split-8.png';
-import unlockedImg5_9 from '@/src/app/images/unlockedQr/0xc9034c3E7F58003E6ae0C8438e7c8f4598d5ACAA/Split-9.png';
+import unlockedImg5_1 from '@/src/app/images/unlockedQr/0x76c0106bBa123E9b32770b2b34B6d13bf4CfA933/Split-1.png';
+import unlockedImg5_2 from '@/src/app/images/unlockedQr/0x76c0106bBa123E9b32770b2b34B6d13bf4CfA933/Split-2.png';
+import unlockedImg5_3 from '@/src/app/images/unlockedQr/0x76c0106bBa123E9b32770b2b34B6d13bf4CfA933/Split-3.png';
+import unlockedImg5_4 from '@/src/app/images/unlockedQr/0x76c0106bBa123E9b32770b2b34B6d13bf4CfA933/Split-4.png';
+import unlockedImg5_5 from '@/src/app/images/unlockedQr/0x76c0106bBa123E9b32770b2b34B6d13bf4CfA933/Split-5.png';
+import unlockedImg5_6 from '@/src/app/images/unlockedQr/0x76c0106bBa123E9b32770b2b34B6d13bf4CfA933/Split-6.png';
+import unlockedImg5_7 from '@/src/app/images/unlockedQr/0x76c0106bBa123E9b32770b2b34B6d13bf4CfA933/Split-7.png';
+import unlockedImg5_8 from '@/src/app/images/unlockedQr/0x76c0106bBa123E9b32770b2b34B6d13bf4CfA933/Split-8.png';
+import unlockedImg5_9 from '@/src/app/images/unlockedQr/0x76c0106bBa123E9b32770b2b34B6d13bf4CfA933/Split-9.png';
 
 // ID: 0xfAB2...
 import unlockedImg6_1 from '@/src/app/images/unlockedQr/0xfAB2F613D2b4c43AE304860f759575359EaC0566/Split-1.png';
@@ -105,15 +105,15 @@ import img4_7 from '@/src/app/images/lockedQr/0x403B1ecaC8d7AC35Dbfb8Efae59F440a
 import img4_8 from '@/src/app/images/lockedQr/0x403B1ecaC8d7AC35Dbfb8Efae59F440a6F3BEB84/Split-8.png';
 import img4_9 from '@/src/app/images/lockedQr/0x403B1ecaC8d7AC35Dbfb8Efae59F440a6F3BEB84/Split-9.png';
 
-import img5_1 from '@/src/app/images/lockedQr/0xc9034c3E7F58003E6ae0C8438e7c8f4598d5ACAA/Split-1.png';
-import img5_2 from '@/src/app/images/lockedQr/0xc9034c3E7F58003E6ae0C8438e7c8f4598d5ACAA/Split-2.png';
-import img5_3 from '@/src/app/images/lockedQr/0xc9034c3E7F58003E6ae0C8438e7c8f4598d5ACAA/Split-3.png';
-import img5_4 from '@/src/app/images/lockedQr/0xc9034c3E7F58003E6ae0C8438e7c8f4598d5ACAA/Split-4.png';
-import img5_5 from '@/src/app/images/lockedQr/0xc9034c3E7F58003E6ae0C8438e7c8f4598d5ACAA/Split-5.png';
-import img5_6 from '@/src/app/images/lockedQr/0xc9034c3E7F58003E6ae0C8438e7c8f4598d5ACAA/Split-6.png';
-import img5_7 from '@/src/app/images/lockedQr/0xc9034c3E7F58003E6ae0C8438e7c8f4598d5ACAA/Split-7.png';
-import img5_8 from '@/src/app/images/lockedQr/0xc9034c3E7F58003E6ae0C8438e7c8f4598d5ACAA/Split-8.png';
-import img5_9 from '@/src/app/images/lockedQr/0xc9034c3E7F58003E6ae0C8438e7c8f4598d5ACAA/Split-9.png';
+import img5_1 from '@/src/app/images/lockedQr/0x76c0106bBa123E9b32770b2b34B6d13bf4CfA933/Split-1.png';
+import img5_2 from '@/src/app/images/lockedQr/0x76c0106bBa123E9b32770b2b34B6d13bf4CfA933/Split-2.png';
+import img5_3 from '@/src/app/images/lockedQr/0x76c0106bBa123E9b32770b2b34B6d13bf4CfA933/Split-3.png';
+import img5_4 from '@/src/app/images/lockedQr/0x76c0106bBa123E9b32770b2b34B6d13bf4CfA933/Split-4.png';
+import img5_5 from '@/src/app/images/lockedQr/0x76c0106bBa123E9b32770b2b34B6d13bf4CfA933/Split-5.png';
+import img5_6 from '@/src/app/images/lockedQr/0x76c0106bBa123E9b32770b2b34B6d13bf4CfA933/Split-6.png';
+import img5_7 from '@/src/app/images/lockedQr/0x76c0106bBa123E9b32770b2b34B6d13bf4CfA933/Split-7.png';
+import img5_8 from '@/src/app/images/lockedQr/0x76c0106bBa123E9b32770b2b34B6d13bf4CfA933/Split-8.png';
+import img5_9 from '@/src/app/images/lockedQr/0x76c0106bBa123E9b32770b2b34B6d13bf4CfA933/Split-9.png';
 
 // ID: 0xfAB2...
 import img6_1 from '@/src/app/images/lockedQr/0xfAB2F613D2b4c43AE304860f759575359EaC0566/Split-1.png';
@@ -133,7 +133,7 @@ export const unlockedImages = [
   { id: '0x4cd15f2BC9533Bf6faC4ae33C649f138CB601935', images: [unlockedImg2_1, unlockedImg2_2, unlockedImg2_3, unlockedImg2_4, unlockedImg2_5, unlockedImg2_6, unlockedImg2_7, unlockedImg2_8, unlockedImg2_9] },
   { id: '0x92051AefA1617CDd2807B7384044f1F253420905', images: [unlockedImg3_1, unlockedImg3_2, unlockedImg3_3, unlockedImg3_4, unlockedImg3_5, unlockedImg3_6, unlockedImg3_7, unlockedImg3_8, unlockedImg3_9] },
   { id: '0x403B1ecaC8d7AC35Dbfb8Efae59F440a6F3BEB84', images: [unlockedImg4_1, unlockedImg4_2, unlockedImg4_3, unlockedImg4_4, unlockedImg4_5, unlockedImg4_6, unlockedImg4_7, unlockedImg4_8, unlockedImg4_9] },
-  { id: '0xc9034c3E7F58003E6ae0C8438e7c8f4598d5ACAA', images: [unlockedImg5_1, unlockedImg5_2, unlockedImg5_3, unlockedImg5_4, unlockedImg5_5, unlockedImg5_6, unlockedImg5_7, unlockedImg5_8, unlockedImg5_9] },
+  { id: '0x76c0106bBa123E9b32770b2b34B6d13bf4CfA933', images: [unlockedImg5_1, unlockedImg5_2, unlockedImg5_3, unlockedImg5_4, unlockedImg5_5, unlockedImg5_6, unlockedImg5_7, unlockedImg5_8, unlockedImg5_9] },
   { id: '0xfAB2F613D2b4c43AE304860f759575359EaC0566', images: [unlockedImg6_1, unlockedImg6_2, unlockedImg6_3, unlockedImg6_4, unlockedImg6_5, unlockedImg6_6, unlockedImg6_7, unlockedImg6_8, unlockedImg6_9] },
 ];
 
@@ -148,7 +148,7 @@ export const lockedImages = [
   { id: '0x4cd15f2BC9533Bf6faC4ae33C649f138CB601935', images: [img2_1, img2_2, img2_3, img2_4, img2_5, img2_6, img2_7, img2_8, img2_9] },
   { id: '0x92051AefA1617CDd2807B7384044f1F253420905', images: [img3_1, img3_2, img3_3, img3_4, img3_5, img3_6, img3_7, img3_8, img3_9] },
   { id: '0x403B1ecaC8d7AC35Dbfb8Efae59F440a6F3BEB84', images: [img4_1, img4_2, img4_3, img4_4, img4_5, img4_6, img4_7, img4_8, img4_9] },
-  { id: '0xc9034c3E7F58003E6ae0C8438e7c8f4598d5ACAA', images: [img5_1, img5_2, img5_3, img5_4, img5_5, img5_6, img5_7, img5_8, img5_9] },
+  { id: '0x76c0106bBa123E9b32770b2b34B6d13bf4CfA933', images: [img5_1, img5_2, img5_3, img5_4, img5_5, img5_6, img5_7, img5_8, img5_9] },
   { id: '0xfAB2F613D2b4c43AE304860f759575359EaC0566', images: [img6_1, img6_2, img6_3, img6_4, img6_5, img6_6, img6_7, img6_8, img6_9] },
 ];
 
