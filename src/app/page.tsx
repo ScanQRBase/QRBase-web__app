cccd374b067d7fcde156c9df0eff248cf986746d
Ahmed Abdelkafi <ahmed.abdelkafi@bitgrass.com>
2025-07-24 13:16:56 +0000

    add frame config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,19 @@ import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import QrBaseMain from "./components/QrBaseMain";
 import partnerData from '@/src/app/data/partnerData.json';
+import { useMiniKit } from '@coinbase/onchainkit/minikit';
 
 export default function Page() {
+  const { setFrameReady, isFrameReady } = useMiniKit();
   const router = useRouter();
 
+
+  useEffect(() => {
+    if (!isFrameReady) {
+      setFrameReady();
+    }
+  }, [setFrameReady, isFrameReady]);
+ 
   useEffect(() => {
     if (partnerData && partnerData[0]) {
       
