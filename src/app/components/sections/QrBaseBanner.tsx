d66a11fe4d5b3579cb2ffdec0af379f951879066
Ahmed Abdelkafi <ahmed.abdelkafi@bitgrass.com>
2025-08-11 11:50:15 +0000

    update round in banner

--- src/app/components/sections/QrBaseBanner.tsx
+++ src/app/components/sections/QrBaseBanner.tsx
@@ -1,17 +1,20 @@
 import { FaBullhorn } from 'react-icons/fa';
 
+interface QrBaseBannerProps {
+  round: string;
+  isCompleted:boolean;
+}
 
-export function QrBaseBanner() {
+export function QrBaseBanner({ round , isCompleted }: QrBaseBannerProps) {
   return (
-<div className="z-50 fixed top-0 left-0 flex h-10 xs:h-11 w-full flex-wrap items-center justify-center sm:h-13
-  bg-gradient-to-r from-[#50DEF5] via-[#0052FF] to-[#AE80FF] text-white">
-  <div className="flex h-full flex-wrap items-center justify-center text-center text-xs sm:text-sm">
-    <FaBullhorn className="text-white w-4 h-4" />
-    <span className="ml-2 text-white">
-      Round 1 is LIVE!
-    </span>
-  </div>
-</div>
-
+    <div className="z-50 fixed top-0 left-0 flex h-10 xs:h-11 w-full flex-wrap items-center justify-center sm:h-13
+      bg-gradient-to-r from-[#50DEF5] via-[#0052FF] to-[#AE80FF] text-white">
+      <div className="flex h-full flex-wrap items-center justify-center text-center text-xs sm:text-sm">
+        <FaBullhorn className="text-white w-4 h-4" />
+        <span className="ml-2 text-white">
+          {round} is {isCompleted ? "COMPLETED" : "LIVE"}!
+        </span>
+      </div>
+    </div>
   );
 }
